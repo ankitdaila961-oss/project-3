e6a2e95252e1c6113afdd6381268dfff38a2ca90
ankitdaila961-oss <ankitdaila961@gmail.com>
2025-09-27 10:23:15 +0530

    Python code

--- hloo.py
+++ hloo.py
@@ -1,14 +1,3 @@
-# import pandas as pd
-
-# data={
-#     "name":["ankit", "pinky", "Aashu", "Priya"],
-#     "adress":["chandigarh", "gurgaon", "surajgarh", "pilani"],
-#     "Gender":["M","F","N","F"],
-#     "Age":[20,21,98,100]
-# }
-# df=pd.DataFrame(data)
-
-# print(df)
 import pandas as pd
 
 df=pd.read_csv("Details.csv")
@@ -16,6 +5,7 @@ print(df)
 
 df=pd.read_csv("detls.csvv.csv")
 print(df)
+
 
 
 
