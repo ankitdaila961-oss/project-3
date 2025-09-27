e615847b10ef51bcc3210589f3c7c755a307cc12
ankitdaila961-oss <ankitdaila961@gmail.com>
2025-09-27 10:13:29 +0530

    Update hloo.py

--- hloo.py
+++ hloo.py
@@ -1,10 +1,15 @@
+# import pandas as pd
+
+# data={
+#     "name":["ankit", "pinky", "Aashu", "Priya"],
+#     "adress":["chandigarh", "gurgaon", "surajgarh", "pilani"],
+#     "Gender":["M","F","N","F"],
+#     "Age":[20,21,98,100]
+# }
+# df=pd.DataFrame(data)
+
+# print(df)
 import pandas as pd
 
-data={
-    "name":["ankit", "pinky", "Aashu", "Priya"],
-    "adress":["chandigarh", "gurgaon", "surajgarh", "pilani"],
-    "Gender":["M","F","N","F"],
-    "Age":[20,21,98,100]
-}
-df=pd.DataFrame(data)
+df=pd.read_csv("Details.csv")
 print(df)
